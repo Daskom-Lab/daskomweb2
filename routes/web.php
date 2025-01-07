1b0cafcfba96429909e2c584e1ce4964a836ac74
Zaidanfzn <zaidanfauzan.edu@gmail.com>
2025-01-08 00:51:29 +0700

    improve environment file handling

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,6 @@ Route::get('/about', function () {
     return Inertia::render('PagesPraktikan/AboutPage');
 })->name('about');
 
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
