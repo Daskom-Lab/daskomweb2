79c83882a9a705dc85dc90fda331dd86055c206e
Zaidanfzn <zaidanfauzan.edu@gmail.com>
2025-01-10 12:59:04 +0700

    Clean up unused code

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
 Route::get('/', function () {
     return Inertia::render('LandingPage');
 })->name('landing');
