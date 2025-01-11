b9171201297a151460d9b089356524a0572da447
unknown <alizahra2403@gmail.com>
2025-01-11 20:03:50 +0700

    Add feature to view assistant plotting in modal

--- resources/js/Components/ComponentsAssistants/TablePlottingan.jsx
+++ resources/js/Components/ComponentsAssistants/TablePlottingan.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-
 import ModalDeletePlottingan from "./ModalDelatePlottingan";
 import ModalEditPlotting from "./ModalEditPlottingan";
+import ModalPlotTeam from "./ModalPlotTeam";
 import trashIcon from "../../../assets/nav/Icon-Delete.svg";
 import editIcon from "../../../assets/nav/Icon-Edit.svg";
 
 export default function TablePlottingan() {
     const [isModalOpenDelate, setIsModalOpenDelate] = useState(false);
-    const [isModalOpenEdit, setIsModalOpenEdit] = useState(false); // State for edit modal
-    const [message, setMessage] = useState(""); // To display success message
+    const [isModalOpenEdit, setIsModalOpenEdit] = useState(false);
+    const [isModalOpenPlot, setIsModalOpenPlot] = useState(false);
+    const [message, setMessage] = useState("");
 
     const handleOpenModalDelate = () => {
         setIsModalOpenDelate(true);
@@ -27,9 +28,16 @@ export default function TablePlottingan() {
     };
 
     const handleConfirmDelete = () => {
-        setMessage("Shift berhasil dihapus"); // Set success message
+        setMessage("Shift berhasil dihapus");
     };
 
+    const handleOpenModalPlot = () => {
+        setIsModalOpenPlot(true);
+    };
+
+    const handleCloseModalPlot = () => {
+        setIsModalOpenPlot(false);
+    };
     return (
         <div className="mt-8">
             {/* Scrollable table container */}
@@ -68,7 +76,7 @@ export default function TablePlottingan() {
 
                                         {/* Edit button */}
                                         <button
-                                            onClick={handleOpenModalEdit} 
+                                            onClick={handleOpenModalEdit}
                                             className="flex justify-center items-center p-2 text-darkBrown font-semibold hover:underline transition-all"
                                         >
                                             <img
@@ -77,6 +85,31 @@ export default function TablePlottingan() {
                                                 alt="edit icon"
                                             />
                                             Edit
+                                        </button>
+                                        {/* plot button */}
+                                        <button
+                                            onClick={handleOpenModalPlot}
+                                            className="flex justify-center items-center p-2 bg-forestGreen text-white rounded hover:bg-deepForestGreen transition-all"
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth="2"
+                                                stroke="currentColor"
+                                                className="w-4 h-4"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                                />
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                                />
+                                            </svg>
                                         </button>
                                     </div>
                                 </td>
@@ -90,13 +123,16 @@ export default function TablePlottingan() {
             {isModalOpenDelate && (
                 <ModalDeletePlottingan
                     onClose={handleCloseModalDelate}
-                    onConfirm={handleConfirmDelete} 
-                    message={message} 
+                    onConfirm={handleConfirmDelete}
+                    message={message}
                 />
             )}
 
             {isModalOpenEdit && (
-                <ModalEditPlotting onClose={handleCloseModalEdit} /> 
+                <ModalEditPlotting onClose={handleCloseModalEdit} />
+            )}
+            {isModalOpenPlot && (
+                <ModalPlotTeam onClose={handleCloseModalPlot} />
             )}
         </div>
     );
