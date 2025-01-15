ee5cfac79a52a8fa2d47837beebf39d009afd106
Zaidan <118703780+Zaidanfzn@users.noreply.github.com>
2025-01-15 19:48:41 +0700

    fix: Use absolute path for login
    Co-authored-by: Muhammad Fakhri Putra Supriyadi <fakhrip@protonmail.com>

--- resources/js/Components/ComponentsPraktikans/MainLanding.jsx
+++ resources/js/Components/ComponentsPraktikans/MainLanding.jsx
@@ -6,7 +6,7 @@ export default function MainLanding() {
             <div className="text-center">
                 <h1 className="text-deepForestGreen font-bold text-3xl">Welcome To</h1>
                 <h1 className="text-deepForestGreen font-bold text-[55px]">Daskom Laboratory</h1>
-                <Link href="login">
+                <Link href="/login">
                     <button className="mt-6 px-12 py-2 bg-deepForestGreen text-xl text-white font-bold border-4 border-white rounded-2xl shadow-lg shadow-black hover:bg-darkOliveGreen transition duration-300">
                         Get Started
                     </button>
