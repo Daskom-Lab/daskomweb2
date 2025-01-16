bfdd2dd6ecb625c1e7bec46313941a9da6d64d56
unknown <alizahra2403@gmail.com>
2025-01-16 22:04:17 +0700

    Add button reset soal &  update role di manage role

--- resources/js/Components/ComponentsAssistants/ContentSoal.jsx
+++ resources/js/Components/ComponentsAssistants/ContentSoal.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import SoalInputForm from "./FormInputSoal";
+import ButtonResetInputSoal from "./ModalResetInputSoal";
 
 export default function ContentSoal() {
+    const [showModalReset, setShowModalReset] = useState(false);
+
+    const handleOpenModalReset = () => setShowModalReset(true);
+    const handleCloseModalReset = () => setShowModalReset(false);
+
     return (
         <section>
             {/* header input soal */}
@@ -10,15 +17,26 @@ export default function ContentSoal() {
                         Manage Soal
                     </h6>
                 </div>
+
+                {/* Button Reset Plottingan */}
+                <button
+                    onClick={handleOpenModalReset}
+                    className="text-white text-md font-semibold px-4 py-1 rounded-md shadow-md bg-redredDark hover:bg-rustyRed transition mt-[2px]"
+                >
+                    Reset Soal
+                </button>
             </div>
 
             {/* all dropdown input soal */}
             <div className="flex gap-5 mt-4 items-start border-2 border-darkBrown rounded-md shadow-md">
                 {/* Panggil komponen dropdown jenis soal */}
-                <div className="overflow-y-auto h-96 w-full"> 
+                <div className="overflow-y-auto h-96 w-full">
                     <SoalInputForm />
                 </div>
             </div>
+
+            {/* Modal Reset input soal */}
+                        {showModalReset && <ButtonResetInputSoal onClose={handleCloseModalReset} />} 
         </section>
     );
 }
