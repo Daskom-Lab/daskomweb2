20cd4770127d60e74e2fda863f530404df1277b5
unknown <alizahra2403@gmail.com>
2025-01-17 07:28:51 +0700

    Add again button pelanggaran

--- resources/js/Components/ComponentsAssistants/ContentPelanggaran.jsx
+++ resources/js/Components/ComponentsAssistants/ContentPelanggaran.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import ButtonResetPelanggaran from "./ModalReserPelanggaran";
 import TablePelanggaran from "./TablePelanggaran";
 
 export default function ContentPelanggaran() {
+    const [showModalReset, setShowModalReset] = useState(false); 
+
+    const handleOpenModalReset = () => setShowModalReset(true);
+    const handleCloseModalReset = () => setShowModalReset(false);
+
     return (
         <section>
             {/* button pelanggaran */}
@@ -8,12 +15,23 @@ export default function ContentPelanggaran() {
                 <div className="border-2 border-darkBrown rounded-md shadow-md ">
                     <h6 className="text-md text-darkBrown text-center py-1 font-semibold px-24">Pelanggaran Assistant</h6>
                 </div>
+
+                 {/* Button Reset Plottingan */}
+                 <button
+                    onClick={handleOpenModalReset}
+                    className="text-white text-md font-semibold px-4 py-1 rounded-md shadow-md bg-redredDark hover:bg-rustyRed transition mt-[2px]"
+                >
+                    Reset Pelanggaran
+                </button>
             </div>
 
             {/* Table data pelanggaran */}
             <div className="">
                 <TablePelanggaran />
             </div>
+
+            {/* Modal Reset Plottingan */}
+                        {showModalReset && <ButtonResetPelanggaran onClose={handleCloseModalReset} />} 
         </section>
     );
 }
